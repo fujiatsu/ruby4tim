7a96396ab18c88d08c3415ad4b2a2c9192405318
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-27 19:22:53 +0900

    Ans to exercise15(3)

---
+++ 1503_wday_output.rb
@@ -0,0 +1,15 @@
+# encooding: UTF-8
+
+wday = {
+  sunday: "日曜日",
+  monday: "月曜日",
+  tuesday: "火曜日",
+  wednesday: "水曜日",
+  thursday: "木曜日",
+  friday: "金曜日",
+  saturday: "土曜日"
+}
+
+wday.each do |key, value|
+  puts "「#{key}」は#{value}のことです。"
+end
