fc19c6451443e656e05d881d172154a2c6eaac0c
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-27 19:21:34 +0900

    Ans to exercise15(1)

---
+++ 1501_wday.rb
@@ -0,0 +1,15 @@
+# encooding: UTF-8
+
+wday = {
+  sunday: "日曜日",
+  monday: "月曜日",
+  tuesday: "火曜日",
+  wednesday: "水曜日",
+  thursday: "木曜日",
+  friday: "金曜日",
+  saturday: "土曜日"
+}
+
+puts wday[:sunday]
+puts wday[:monday]
+puts wday[:saturday]
