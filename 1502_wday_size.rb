b79830fe35009f20102290b6d5bd679bacc8728e
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-27 19:22:15 +0900

    Ans to exercise15(2)

---
+++ 1502_wday_size.rb
@@ -0,0 +1,13 @@
+# encooding: UTF-8
+
+wday = {
+  sunday: "日曜日",
+  monday: "月曜日",
+  tuesday: "火曜日",
+  wednesday: "水曜日",
+  thursday: "木曜日",
+  friday: "金曜日",
+  saturday: "土曜日"
+}
+
+puts "wday has #{wday.size} pairs"
