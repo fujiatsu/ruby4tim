e39b93a3a8fa19fed8fd9c49e4abca6c8e1450ba
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-29 14:44:38 +0900

    Fixed ans to exercise13(3)

--- 1303_array_a3.rb
+++ 1303_array_a3.rb
@@ -1,11 +1,7 @@
-a = Array.new
+a = [*1..100]
 
-100.times do |i|
-  a << i + 1
-end
-
-a3 = a.reject{|item| item % 3 != 0}
-a.delete_if{|item| item % 3 != 0}
+a3 = a.select{|item| item % 3 == 0}
+a.delete_if{|item| item % 3 == 0}
 
 print "a3 = #{a3}\n"
 print "a = #{a}\n"
