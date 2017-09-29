ae7fe8ee5130ed714fc930a8183541031e4555bf
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-29 16:26:20 +0900

    Fixed ans to exercise14(5)

--- 1405_count_elements.rb
+++ 1405_count_elements.rb
@@ -1,23 +1,6 @@
-str = "Ruby is an object oriented progamming language"
-strAry = []
-length = 0
-while length < str.length
-  strAry << str[length]
-  length += 1
-end
-strAry.sort!
-elm = strAry[0]
-count = ""
-while !strAry.empty?
-  shiftAry = strAry.shift
-  if shiftAry == elm
-    count += "*"
-  else
-    puts "\'#{elm}\': #{count}"
-    elm = shiftAry
-    count = "*"
-  end
-  if strAry.empty?
-    puts "\'#{elm}\': #{count}"
-  end
-end
+str = "Ruby is an object oriented programming language"
+hash = Hash.new(0)
+
+str.split("").sort.each{|i| hash[i] += 1 }
+
+hash.each {|k, v| print "#{k}:#{"*" * v}\n" }
