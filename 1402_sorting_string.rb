1f950dc97c93c45387590a2068554c83ed13b2fc
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-29 14:54:48 +0900

    Fixed ans to exercise14(2)

--- 1402_sorting_string.rb
+++ 1402_sorting_string.rb
@@ -1,4 +1,3 @@
-str = "Ruby is an object oriented progamming language"
-ary = str.split(/ /)
-sortedAry = ary.sort
-puts sortedAry
+str = "Ruby is an object oriented programming language"
+
+puts str.split(/ /).sort
