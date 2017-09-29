0cf1b84c9d7051cfb93126c1eeda823be829c615
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-09-29 14:53:10 +0900

    Fixed ans to exercise13(7)

--- 1307_sum_array.rb
+++ 1307_sum_array.rb
@@ -1,29 +1,8 @@
 nums1 = [1, 2, 3]
 nums2 = [4, 6, 8]
 
-def sum_array(nums1, nums2)
-  nums3 = Array.new
-  i = 0
-  if nums1.length != nums2.length
-    return "infeasible"
-  end
-  while i < nums1.length
-    nums3 << nums1[i] + nums2[i]
-    i += 1
-  end
-  return nums3
-end
-
 def sum_array2(nums1, nums2)
-  nums4 = Array.new
-  if nums1.length != nums2.length
-    return "infeasible"
-  end
-  nums1.zip(nums2){|a, b|
-    nums4 << a + b
-  }
-  return nums4
+  nums1.zip(nums2).map{|a, b| a + b }
 end
 
-puts "num3 = #{sum_array(nums1, nums2)}"
-puts "num4 = #{sum_array2(nums1, nums2)}"
+puts "num3 = #{sum_array2(nums1, nums2)}"
