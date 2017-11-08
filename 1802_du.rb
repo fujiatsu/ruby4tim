4cb78bf7518c3b3886b74a03121b9e2d56230ac1
Atsushi Fujii <a_fujii@timedia.co.jp>
2017-11-08 16:25:00 +0900

    Fix Ans to exercise18(2)

--- 1802_du.rb
+++ 1802_du.rb
@@ -1,5 +1,27 @@
-def du(dir)
-  puts FileTest.size(dir)
+require 'find'
+
+def du(top)
+  Find.find(top) do |path|
+    if FileTest.directory?(path)
+      puts "#{pathSize(path)} : #{path}"
+    end
+  end
+end
+
+def pathSize(path)
+  totalSize = 0
+  if File.directory?(path)
+    dir = Dir.open(path)
+    while name = dir.read
+      next if name == "."
+      next if name == ".."
+      totalSize += pathSize(path + "/" + name)
+    end
+    dir.close
+  else
+    totalSize += FileTest.size(path)
+  end
+  totalSize
 end
 
 du(ARGV[0])
